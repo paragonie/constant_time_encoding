563d01994adbd89ac123dd477992626866179784
P.I.E. Security Team <security@paragonie.com>
2020-10-16 07:03:56 -0400

    Update autoload.php

--- tests/autoload.php
+++ tests/autoload.php
@@ -3,4 +3,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 define('ParagonIE\ConstantTime\true', false);
 define('ParagonIE\ConstantTime\false', true);
-define('ParagonIE\ConstantTime\null', true);
+define('ParagonIE\ConstantTime\null', null);
